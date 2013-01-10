7514445086e55899b56f1c0d2f5c1974768f2ac7
Rohit Rox <link2j.roy@gmail.com>
2013-01-10 15:45:26 +0545

    logic implemented for pulling and rendering task in iframe

--- public/init.js
+++ public/init.js
@@ -1,6 +1,6 @@
 function log(data){ console.log(data); }
 var Main = Backbone.Model.extend({
-  defaults:{ template: ""},
+  defaults:{ template: "", first_run: true },
   urlRoot: "",
   url: function() {
       var base = '/work';
@@ -15,24 +15,50 @@ var MainView = Backbone.View.extend({
   },
   render_in_iframe: function(){
     var me = this;
-    var tmp;
     var template = me.model.get('template');
-    $.get('/template/'+template, function(data){
-      tmp = $(data);
-    }).success( function(){
-      var template = tmp.find('#data_container').html();
-      var render = Mustache.to_html(template, me.model.get('inputs'));
-      data = tmp.find('#data_container').html(render);
-      me.$el.contents().find('body').html(tmp);
-      var height = document.getElementById('render_frame').contentWindow.document.body.scrollHeight;
-      me.adjust_iframe(height);
-    });
+    if( this.model.get('first_run') ){
+      this.$el.attr('src', '/'+template+'.html');
+      this.model.set('first_run', false);
+      LocalDS.set('current', this.model.get('template')); 
+      this.$el.on('load', function(){ me.render_data(); });
+
+    }else{
+      if ( !(LocalDS.get('current') == template) )
+      {
+        LocalDS.set("current", template);
+        this.$el.attr('src', '/'+template+'.html');
+      }
+      this.$el.on('load', function(){ me.render_data(); });
+    }
+   
+  },
+  render_data: function(){
+    var frame = this.$el;
+    var template = frame.contents().find('#data_container').html();
+    var render = Mustache.to_html(template, this.model.get('inputs'));
+    frame.contents().find('#data_container').html(render);
+    this.adjust_iframe();
   },
   adjust_iframe: function(height){
-    log(height);
-    this.$el.height(height);
+    this.$el.height(800);
+  },
+  next_task: function(){
+    mod.fetch();
+
   }
 });
+
+var LocalDS = {
+  set: function(key, value){
+    return localStorage.setItem(key, value);
+  },
+  get: function(key){ 
+    return localStorage.getItem(key);
+  },
+  find: function(key){
+    return localStorage.hasOwnProperty(key);
+  }
+}
 
 var mod = new Main();
 var vw = new MainView({ model: mod});
